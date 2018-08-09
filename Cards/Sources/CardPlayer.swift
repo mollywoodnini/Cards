e5d36aae029b6f488a4d4f03241272485df845db
Tan Nghia La <tannghia.la@rewe-digital.com>
2018-08-09 15:27:45 +0200

    fixed broken stuff

--- Cards/Sources/CardPlayer.swift
+++ Cards/Sources/CardPlayer.swift
@@ -124,7 +124,7 @@ import Player
         // Player Init
         player.playerDelegate = self
         player.playbackDelegate = self
-        player.fillMode = PlayerFillMode.resizeAspectFill.avFoundationType
+
         if let url = videoSource { player.url = url }
         else { print("CARDS: Something wrong with the video source URL") }
        
@@ -284,6 +284,10 @@ import Player
 
 // Player Delegates
 extension CardPlayer: PlayerDelegate {
+    public func player(_ player: Player, didFailWithError error: Error?) {
+        
+    }
+    
     public func playerReady(_ player: Player) {
         
         player.view.addSubview(playPauseV)
